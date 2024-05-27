2523988d1d49ffe79279cf76193a9322c71bcdf8
11best <asama_b@outlook.com>
2024-05-27 21:35:45 +0700

    separate layout for login page and management page group

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Kanit } from "next/font/google";
 import "./globals.css";
-import Sidebar from "@/components/Sidebar";
-import Header from "@/components/Header";
 
 const kanit = Kanit({ weight: "300", subsets: ["thai"] });
 
@@ -19,13 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={kanit.className}>
-        <div className="flex min-h-screen mx-auto max-w-screen-xl px-8">
-          <Sidebar />
-          <div className="flex-1 flex flex-col">
-            <Header />
-            <main>{children}</main>
+        <main>
+          <div className="min-h-screen mx-auto max-w-screen-xl px-8 py-2">
+            {children}
           </div>
-        </div>
+        </main>
       </body>
     </html>
   );
